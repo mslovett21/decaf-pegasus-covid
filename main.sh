fc5a69b5fe11b6b1120cef65cc2d63b6b68073af
Tu Mai Anh Do <tudo@usc.edu>
2020-11-25 03:36:00 -0800

    Check number of input arguments

--- main.sh
+++ main.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 #SBATCH --qos=debug
 #SBATCH --time=00:30:00
-#SBATCH --nodes=2
+#SBATCH --nodes=3
 #SBATCH --constraint=haswell
+
+if [ "$#" -ne 1 ]; then
+    echo "./main.sh number_of_workers"
+    exit 2
+fi
 
 # Parameters
 NWORKERS=$1
-NTIMES=2  #Change to desired number of times each version should run
+
+#Change to desired number of times each version should run
+NTIMES=2
 
 # Load environment script
 . $HOME/application/envs/pegasus.sh
