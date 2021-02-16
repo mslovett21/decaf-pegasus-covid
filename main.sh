aa3c11815b268e2bc9470187c0bcc4ff3a35ed08
Tu Mai Anh Do <tudo@usc.edu>
2021-02-16 12:54:32 -0800

    main script

--- main.sh
+++ main.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
-#SBATCH --qos=debug
-#SBATCH --time=00:30:00
-#SBATCH --nodes=3
-#SBATCH --constraint=haswell
+#SBATCH -A m2187
+#SBATCH -C gpu
+#SBATCH -G 16
+#SBATCH -c 80
+#SBATCH -t 02:00:00
 
-if [ "$#" -ne 1 ]; then
-    echo "./main.sh number_of_workers"
+if [ "$#" -ne 4 ]; then
+    echo "./main.sh NTRIALS NWORKERS NTHREADS_PER_WORKER NJOBS"
     exit 2
 fi
 
 # Parameters
-NWORKERS=$1
+NTRIALS=$1
+NWORKERS=$2
+NTHREADS_PER_WORKER=$3
+NJOBS=$4
 
 #Change to desired number of times each version should run
-NTIMES=2
+NTIMES=1
+
+echo "Number of trials: ${NTRIALS}"
+echo "Number of workers: ${NWORKERS}"
+echo "Number of threads per worker: ${NTHREADS_PER_WORKER}"
+echo "Number of running times: ${NTIMES}"
+echo "Number of Python threads: ${NJOBS}"
 
 # Load environment script
-. $HOME/application/envs/pegasus.sh
+. $HOME/application/envs/decaf.sh
 
 # DB version
-echo "Run database version with ${NWORKERS} workers in ${NTIMES} times"
+echo "Run database version"
 cd db
-./run ${NWORKERS} ${NTIMES}
+./run ${NTRIALS} ${NWORKERS} ${NTHREADS_PER_WORKER} ${NJOBS} ${NTIMES}
 cd .. 
 
 # Decaf version
-echo "Run Decaf version with ${NWORKERS} workers in ${NTIMES} times"
-cd decaf
-./run ${NWORKERS} ${NTIMES}
-cd ..
+#echo "Run Decaf version with ${NWORKERS} workers in ${NTIMES} times"
+#cd decaf
+#./run ${NTRIALS} ${NWORKERS} ${NTHREADS_PER_WORKER} ${NTIMES}
+#cd ..
